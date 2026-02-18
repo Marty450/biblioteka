60d9d217107c5b50a972f06e64cef414354bf103
Marty450 <kavrakovmartin3@gmail.com>
2026-02-18 14:07:53 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+if "books" not in st.session_state:
+    st.session_state.books = []
+
+title = st.text_input("Заглавие")
+author = st.text_input("Автор")
+
+if st.button("Добави книга"):
+    book = {
+        "title": title,
+        "author": author
+    }
+    st.session_state.books.append(book)
+    st.success("Книгата е добавена!")
+
+st.write("Списък с книги:")
+st.write(st.session_state.books)
